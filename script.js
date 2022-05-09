83d69936e32880f512e9bd1ba985eb504c0b038e
Neal <tnharvey@gmail.com>
2022-05-09 13:32:12 +0000

    refactor" added updateOrientation, restructured userAction

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /* TO DO
 1. Reset view after each challenge
 2. update messages
-3. create function to check which options have content, set 3d card to glow.
-4. Update score/health (waiting for feedback from charles)
+3. Update End Scenario to show ending video popup
+4. create function to check which options have content, set 3d card to glow.
+5. Update score/health (waiting for feedback from charles)
 - Refactor popup system to use array/object and allow for tracking the last popup and close it or close all popups
 Might also be able to use an object to load and track all popup info at once and load/unload individual ones
 */
@@ -333,18 +334,59 @@ function userAction (input) {
   if (uAction !== "") {
     if(uAction.search("face-")==0){
       if(uAction == "face-front"||uAction == "face-right"||uAction == "face-left"){
-        cube.classList.remove(cube.classList[1]);
-        cube.classList.add(uAction);
+        //updateOrientation(uAction);
         document.getElementById("cards").style.visibility="visible";
         document.getElementById("backButton").style.opacity="0";
         if (uAction=="face-front" && document.getElementById("afssDoor01").classList.contains("afssDoor-open")){
           document.getElementById("afssDoor01").classList.remove("afssDoor-open");
         }
+        //Room orientation
+        if (orientation=="face-front") {
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-left");
+          }
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-right");
+          }
+        }
+        else if (orientation=="face-left"){
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-lBack");
+          }
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-front");
+          }
+        }
+        else if (orientation=="face-lBack"){
+          cube.addEventListener("transitionend",switchOrientation);
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-lRight");
+          }
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-left");
+          }
+        }
+        else if (orientation=="face-right"){
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-front");
+          }
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-rBack");
+          }
+        }
+        else if (orientation=="face-rBack"){
+          cube.addEventListener("transitionend",switchOrientation);
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-right");
+          }
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-rLeft");
+          }
+        }
       }
       else {
         document.getElementById("backButton").dataset.targetDir = cube.classList[1];
-        cube.classList.remove(cube.classList[1]);
-        cube.classList.add(uAction);
+        updateOrientation(uAction);
         document.getElementById("cards").style.visibility="hidden";
         document.getElementById("backButton").style.opacity=1;
         if(uAction=="face-equipment") {
@@ -366,84 +408,37 @@ function userAction (input) {
           currentPopup = "popupOption04"
           cube.addEventListener("transitionend",transitionPopup);
         }
-      }
-    }
-    //Room orientation
-    if (orientation=="face-front") {
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-front");
-        cube.classList.add("face-left");
-      }
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-front");
-        cube.classList.add("face-right");
-      }
-    }
-    else if (orientation=="face-left"){
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-left");
-        cube.classList.add("face-lBack");
-      }
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-left");
-        cube.classList.add("face-front");
-      }
-    }
-    else if (orientation=="face-lBack"){
-      cube.addEventListener("transitionend",switchOrientation);
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-lBack");
-        cube.classList.add("face-lRight");
-      }
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-lBack");
-        cube.classList.add("face-left");
-      }
-    }
-    else if (orientation=="face-right"){
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-right");
-        cube.classList.add("face-front");
-      }
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-right");
-        cube.classList.add("face-rBack");
-      }
-    }
-    else if (orientation=="face-rBack"){
-      cube.addEventListener("transitionend",switchOrientation);
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-rBack");
-        cube.classList.add("face-right");
-      }
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-rBack");
-        cube.classList.add("face-rLeft");
-      }
-    }
-    else if (orientation=="face-equipment"){
-      if(uAction === "ArrowLeft") {}
-      else if (uAction === "ArrowRight"){}
-    }
-    else if (orientation=="face-resources"){
-      if(uAction === "ArrowLeft") {}
-      else if (uAction === "ArrowRight"){}
-    }
-    else if (orientation=="face-computer"){
-      if(uAction === "ArrowLeft") {
-        cube.classList.remove("face-computer");
-        cube.classList.add("face-references");
-      }
-      else if (uAction === "ArrowRight"){}
-    }
-    else if (orientation=="face-references"){
-      if(uAction === "ArrowLeft") {}
-      else if (uAction === "ArrowRight"){
-        cube.classList.remove("face-references");
-        cube.classList.add("face-computer");
+        else if (orientation=="face-equipment"){
+          if(uAction === "ArrowLeft") {}
+          else if (uAction === "ArrowRight"){}
+        }
+        else if (orientation=="face-resources"){
+          if(uAction === "ArrowLeft") {}
+          else if (uAction === "ArrowRight"){}
+        }
+        else if (orientation=="face-computer"){
+          if(uAction === "ArrowLeft") {
+            updateOrientation("face-references");
+          }
+          else if (uAction === "ArrowRight"){}
+        }
+        else if (orientation=="face-references"){
+          if(uAction === "ArrowLeft") {}
+          else if (uAction === "ArrowRight"){
+            updateOrientation("face-computer");
+          }
+        }
       }
     }
   }
+}
+
+function updateOrientation(newOrientation) {
+  var cube = document.querySelector('#scene1');
+  
+  cube.classList.remove(findInList(cube.classList,"face-"));
+  cube.classList.add(newOrientation);
+  
 }
 
 function switchOrientation(e) {
